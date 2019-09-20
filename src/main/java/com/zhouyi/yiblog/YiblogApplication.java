9e2294850eed03c018f1b938f02ecac4208a0e23
Yi Zhou <zhouyi119119@gmail.com>
2019-09-20 22:06:51 +0800

    后端模块初始化”

---
+++ src/main/java/com/zhouyi/yiblog/YiblogApplication.java
@@ -0,0 +1,13 @@
+package com.zhouyi.yiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhouyi/yiblog/YiblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhouyi.yiblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YiblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

cf5425087c29ef274eab5696f67243364539a5e0
Yi Zhou <zhouyi119119@gmail.com>
2019-09-21 19:55:05 +0800

    前台静态页面切换为thymeleaf模板

--- src/main/java/com/zhouyi/yiblog/web/IndexController.java
+++ src/main/java/com/zhouyi/yiblog/web/IndexController.java
@@ -1,22 +1,40 @@
 package com.zhouyi.yiblog.web;
 
 
-import com.zhouyi.yiblog.NotFoundException;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name) {
+    @GetMapping("/")
+    public String index() {
 //        int i = 9 / 0;
 //        String blog = null;
 //        if (blog == null){
 //            throw new NotFoundException("博客不存在");
 //        }
-        System.out.println("--------index-------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/types")
+    public String types() {
+        return "types";
+    }
+
+    @GetMapping("/tags")
+    public String tags() {
+        return "tags";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
     }
 }
